adda7fc824ac4480006f75db291f07945c776ca4
Lusjen <lusjenh@mail.ru>
2018-04-16 09:21:26 +0300

    change files

--- src/js/lib/video.js
+++ src/js/lib/video.js
@@ -8,7 +8,15 @@
 
 
 // // });
-// $('.promo-content__wrap-video').click(function() {
-//   	 $('.video').addClass('open');
-//   	 $('.js-if').attr('src','https://www.youtube.com/embed/2se9Thz5zyg?ecver=2');
-// });
+var promoVideo = $('.js-promo-video');
+promoVideo.click(function() {
+  	 $('.video').addClass('open');
+  	 $('body').addClass('open');
+  	 $('.js-if').attr('src','https://www.youtube.com/embed/2se9Thz5zyg?ecver=2');
+});
+
+$('.js-back-video').click(function() {
+  	 $('.video').removeClass('open');
+  	 $('body').removeClass('open');
+  	 $('.js-if').attr('src','');
+});
